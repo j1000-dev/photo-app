bfd00b4dbdd5a084502d3716f67f60da9124a1ca
Jill Vina <jvina@my.bcit.ca>
2023-10-24 20:10:21 -0700

    Add icons

---
+++ client/src/components/icons/MenuIcon.jsx
@@ -0,0 +1,20 @@
+export function MenuIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      width="25px" 
+      height="25px"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+      />
+    </svg>
+  );
+}
